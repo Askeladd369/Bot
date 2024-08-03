4bacf3c3162fe081f366c9b64066765de5f20f93
Said <saiddixonjack2013@hotmail.com>
2024-08-02 21:19:56 -0600

    Primer commit

--- config.py
+++
@@ -1,5 +0,0 @@
-# config.py
-api_id = "22823293"
-api_hash = "c110fb4d3ba8473643b8e33e1c81be1d"
-bot_token = "7069733149:AAGxIrOsBBHFLLLgj9OoAOAsn4dNdjwI_Gg"
-admin_id = 1142604997  # Tu ID de Telegram
