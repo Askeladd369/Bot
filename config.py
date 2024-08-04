9453f36110511413e8fd422795c510688e822c0b
Askeladd369 <146481623+Askeladd369@users.noreply.github.com>
2024-08-03 18:29:47 -0600

    Add files via upload

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 api_id = "22823293"
 api_hash = "c110fb4d3ba8473643b8e33e1c81be1d"
 bot_token = "7069733149:AAGxIrOsBBHFLLLgj9OoAOAsn4dNdjwI_Gg"
-admin_id = 1142604997  # Tu ID de Telegram
+admin_id = [1142604997, 1209577470]  # Tu ID de Telegram
